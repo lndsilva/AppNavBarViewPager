6c1516bfa442bde63b5c4ea2036d884eba9bd5e3
lndsilva <lndsilva@hotmail.com>
2026-05-15 21:06:13 -0300

    menu drawer navigation view com navegação viewpager2

--- app/src/main/java/br/com/etecia/myapp/NavDrawerActivity.java
+++ app/src/main/java/br/com/etecia/myapp/NavDrawerActivity.java
@@ -2,8 +2,10 @@ package br.com.etecia.myapp;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,23 +30,83 @@ public class NavDrawerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nav_drawer_layout);
-        ViewCompat.setOnApplyWindowInsetsListener(
-                findViewById(R.id.drawerLayout),
-                (view, insets) -> {
-                    Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-                    view.setPadding(0, systemBars.top, 0, 0);
-                    return insets;
-                });
+
         drawerLayout = findViewById(R.id.drawerLayout);
         navigationView = findViewById(R.id.navViewDrawer);
         toolbar = findViewById(R.id.toolBarDrawer);
         viewPager = findViewById(R.id.viewPagerDrawerNav);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-          this,
-          drawerLayout,
-          toolbar,R.string.open,
+                this,
+                drawerLayout,
+                toolbar, R.string.open,
                 R.string.close
         );
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        AdapterViewPagerNavView adapter = new AdapterViewPagerNavView(this);
+
+        viewPager.setAdapter(adapter);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.mHome) {
+                    viewPager.setCurrentItem(0);
+                    return true;
+                }
+                if (menuItem.getItemId() == R.id.mDashboard) {
+                    viewPager.setCurrentItem(1);
+                    return true;
+                }
+                if (menuItem.getItemId() == R.id.mSettings) {
+                    viewPager.setCurrentItem(2);
+                    return true;
+                }
+                if (menuItem.getItemId() == R.id.mProfile) {
+                    viewPager.setCurrentItem(3);
+                    return true;
+                }
+                if (menuItem.getItemId() == R.id.mUsers) {
+                    viewPager.setCurrentItem(4);
+                    return true;
+                }
+                drawerLayout.closeDrawers();
+                return false;
+            }
+        });
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                switch (position) {
+                    case 0:
+                        navigationView.setCheckedItem(R.id.mHome);
+                        toolbar.setTitle("Home");
+                        break;
+                    case 1:
+                        navigationView.setCheckedItem(R.id.mDashboard);
+                        toolbar.setTitle("Dashboard");
+                        break;
+                    case 2:
+                        navigationView.setCheckedItem(R.id.mSettings);
+                        toolbar.setTitle("Settings");
+                        break;
+                    case 3:
+                        navigationView.setCheckedItem(R.id.mProfile);
+                        toolbar.setTitle("Profile");
+                        break;
+                    case 4:
+                        navigationView.setCheckedItem(R.id.mUsers);
+                        toolbar.setTitle("Users");
+                        break;
+                }
+            }
+        });
+
+
     }
 }
